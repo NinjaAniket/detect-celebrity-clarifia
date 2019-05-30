e2d7ec503f1fa0a5f2744f9032c46333084313d2
NinjaAniket <aniketvnagapure@gmail.com>
2019-05-30 15:34:19 +0530

    Updates

---
+++ precache-manifest.16cef82823fe7b3d4318716f7c72c118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d493cb9c872249d5fe104673e256218",
+    "url": "/detect-celebrity-clarifia/index.html"
+  },
+  {
+    "revision": "39e8bac8d6336e0695ef",
+    "url": "/detect-celebrity-clarifia/static/css/main.b733af07.chunk.css"
+  },
+  {
+    "revision": "8861b651b76e9c143e66",
+    "url": "/detect-celebrity-clarifia/static/js/2.afbb6a85.chunk.js"
+  },
+  {
+    "revision": "39e8bac8d6336e0695ef",
+    "url": "/detect-celebrity-clarifia/static/js/main.091c1b4d.chunk.js"
+  },
+  {
+    "revision": "e2ffe1164626c8e0b830",
+    "url": "/detect-celebrity-clarifia/static/js/runtime~main.907a239f.js"
+  }
+]);
